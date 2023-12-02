60890ed0ae43f08b0405dee2aa048b2ee661c7a4
Maruf Hossain <108384673+Marufhossain112@users.noreply.github.com>
2023-12-02 07:31:45 +0600

    save form data to localStorage

--- src/pages/FirstPage/FirstPage.tsx
+++ src/pages/FirstPage/FirstPage.tsx
@@ -1,7 +1,81 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Box, Button, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+
 const FirstPage = () => {
-  return <div>
-    <p>This is FirstPage</p>
-  </div>;
+
+  useEffect(() => {
+    // Load form data from localStorage on component mount
+    const savedFormData = JSON.parse(localStorage.getItem('formData') as string);
+    if (savedFormData) {
+      setFormData(savedFormData);
+    }
+  }, []);
+  const [formData, setFormData] = useState({
+    name: '',
+    phone: '',
+    email: '',
+  });
+
+  const handleNameChange = (event: any) => {
+    setFormData((prevData) => ({ ...prevData, name: event.target.value }));
+  };
+
+  const handlePhoneChange = (event: any) => {
+    setFormData((prevData) => ({ ...prevData, phone: event.target.value }));
+  };
+
+  const handleEmailChange = (event: any) => {
+    setFormData((prevData) => ({ ...prevData, email: event.target.value }));
+  };
+
+  const handleFormSubmit = () => {
+    // console.log("Form data:", formData);
+    // Save the form data to localStorage
+    localStorage.setItem('formData', JSON.stringify(formData));
+    setFormData({
+      name: '',
+      phone: '',
+      email: '',
+    });
+    // You can perform further actions with formData here
+  };
+
+  return (
+    <Box>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: "10px", justifyContent: "center", alignItems: "center", marginTop: "2rem" }}>
+        <TextField
+          label="Name"
+          value={formData.name}
+          onChange={handleNameChange}
+        />
+        <TextField
+          label="Phone Number"
+          value={formData.phone}
+          onChange={handlePhoneChange}
+        />
+        <TextField
+          label="Email"
+          value={formData.email}
+          onChange={handleEmailChange}
+        />
+        <Button
+          variant={"outlined"}
+          sx={{
+            mt: '10px',
+            color: 'black',
+            '&:hover': {
+              bgcolor: 'green', // Change this to the desired hover color
+              color: 'white',
+            },
+          }}
+          onClick={handleFormSubmit}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export default FirstPage;
